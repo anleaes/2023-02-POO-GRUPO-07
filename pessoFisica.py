e24f536fc8a80767bd1fac9ed4999668cdc38cf7
datLukz <el.souza2010@hotmail.com>
2023-11-08 14:53:19 -0300

    Realizada criação da classe Pessoa Fisica

---
+++ pessoFisica.py
@@ -0,0 +1,14 @@
+class PessoaFisica:
+    def __init__(self, nome, cpf, data_nascimento):
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+
+    def exibir_informacoes(self):
+        print(f"Nome: {self.nome}")
+        print(f"CPF: {self.cpf}")
+        print(f"Data de Nascimento: {self.data_nascimento}")
+
+# Exemplo de uso da classe PessoaFisica
+pessoa = PessoaFisica("João da Silva", "123.456.789-00", "01/01/1980")
+pessoa.exibir_informacoes()
