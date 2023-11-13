98ebd3d3d5581985d580ebaa75dfeb60bb3ec939
datLukz <el.souza2010@hotmail.com>
2023-11-13 13:17:14 -0300

    Ajuste do projeto 1 em new Folder

--- pessoaJuridica.py
+++
@@ -1,9 +0,0 @@
-class Pessoa_Juridica: 
-    
-    def __int__(self, nome_empresa, cnpj):
-        self.nome_empresa = nome_empresa
-        self.cnpj = cnpj
-        
-    def exibir_info(self):
-        print(f"Nome da empresa: {self.nome_empresa}")
-        print(f"CNPJ: {self.cnpj}")
