f4cc127a778f46a59547f1758d9c50ba29c85a1a
Andrew-sobrera <andrewsobrera683@gmail.com>
2023-11-14 21:06:09 -0300

    ajustes finais

--- projeto_curso/matricula.py
+++ projeto_curso/matricula.py
@@ -3,9 +3,9 @@ from pessoaFisica import PessoaFisica
 from pessoa import Pessoa
 
 class Matricula:
-    def __init__(self, numero_matricula, data_matricula, curso, pessoaFisica, Pessoa):
+    def __init__(self, numero_matricula, data_matricula, curso, pessoaFisica, PessoaJuridica):
         self.numero_matricula = numero_matricula
         self.data_matricula = data_matricula
         self.curso = curso
         self.aluno = pessoaFisica
-        self.contratante = Pessoa
+        self.contratante = PessoaJuridica
