9cfd7c49150923f9662e1a1805e5d685a1240c06
Andrew-sobrera <andrewsobrera683@gmail.com>
2023-12-07 19:13:55 -0300

    ajuste final

--- apps/lojaMuseu/models.py
+++ apps/lojaMuseu/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from obraDeArte.models import ObraDeArte
+
 # Create your models here.
 
 class LojaMuseu(models.Model):
     name = models.CharField('Nome da Obra:', max_length=100)
     valor = models.CharField('Valor da Obra:', max_length=50)
     quantidade = models.TextField('Quantidade disponivel:', max_length=10)
-    
+    obraDeArte = models.ForeignKey(ObraDeArte, on_delete=models.CASCADE, default=0)
     
     class Meta:
         verbose_name = 'LojaMuseu'
--- apps/obraDeArte/models.py
+++ apps/obraDeArte/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from artistas.models import Artista
+
 # Create your models here.
 
 class ObraDeArte(models.Model):
     name = models.CharField('Nome', max_length=50)
-    Artista = models.CharField('Artista', max_length=50)
     valor = models.TextField('valor', max_length=100)
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE, default=0)
     
     class Meta:
         verbose_name = 'ObraDeArte'
