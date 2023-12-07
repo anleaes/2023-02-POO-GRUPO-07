599199edfbc4d2bada2aa0898ec65dc12e1ede2a
datLukz <el.souza2010@hotmail.com>
2023-12-07 00:45:23 -0300

    ajuste models

--- apps/lojaMuseu/models.py
+++ apps/lojaMuseu/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class LojaMuseu(models.Model):
-    nome = models.CharField('Nome da Obra:', max_length=100)
+    name = models.CharField('Nome da Obra:', max_length=100)
     valor = models.CharField('Valor da Obra:', max_length=50)
     quantidade = models.TextField('Quantidade disponivel:', max_length=10)
     
