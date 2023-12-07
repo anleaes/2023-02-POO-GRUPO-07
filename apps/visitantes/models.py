614f3041023bd3d53ee45ab1dc797c8cb0ffe820
Andrew-sobrera <andrewsobrera683@gmail.com>
2023-12-06 22:18:29 -0300

    ligacao

--- apps/visitantes/models.py
+++ apps/visitantes/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from exposicoes.models import Exposicao
+
 # Create your models here.
 
 class Visitante(models.Model):
     nome = models.CharField('Nome', max_length=50)
     sobrenome = models.CharField('Sobrenome', max_length=50)
     rg = models.IntegerField('rg' ,max_length=10)
+    exposicao = models.ForeignKey(Exposicao, on_delete=models.CASCADE, default=0)
     
     class Meta:
         verbose_name = 'Categoria'
